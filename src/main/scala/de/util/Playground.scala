0be67585de58acf100f9f3754465ca1d465fb3d2
visenger <visenger@gmail.com>
2014-08-16 23:02:07 +0200

    playing around with spark sql

--- src/main/scala/de/util/Playground.scala
+++ src/main/scala/de/util/Playground.scala
@@ -1,13 +1,49 @@
 package de.util
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.SparkContext._
+
 /**
  * Created by visenger on 13/08/14.
  */
+case class Customer(custKey: String, name: String, addr: String, natKey: String, phone: String, acc: String, mrkt: String, comment: String)
+
 object Playground {
 
   def main(args: Array[String]) {
-    println("huhu!")
-    Util.playSound
+
+    val sparkConf: SparkConf = new SparkConf().setMaster("local[4]").setAppName("SPARK")
+    val sc: SparkContext = new SparkContext(sparkConf)
+
+
+
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    import sqlContext.createSchemaRDD
+
+
+    val customers = sc.textFile("/Users/visenger/data/TPC-H/tpch_2_17_0/tables/customer.tbl").map(line => {
+      val Array(custKey, name, addr, natKey, phone, acc, mrkt, comment) = line.split('|')
+      Customer(custKey.toString, name.toString, addr.toString, natKey.toString, phone.toString, acc.toString, mrkt.toString, comment.toString)
+    })
+
+    customers.registerAsTable("customers")
+
+        val automobile = sqlContext.sql("SELECT custKey, name FROM customers WHERE mrkt='AUTOMOBILE'")
+
+        println("automobile count = " + automobile.count())
+
+    //    automobile.map(m => s"cust key: ${m(0)} name: ${m(1)}").foreach(println)
+
+    import sqlContext._
+
+    val anotherAuto = customers.where('mrkt === "AUTOMOBILE").select('name)
+    println("another auto count = " + anotherAuto.count())
+
+    sc.stop()
+
   }
 
 }
+
+
