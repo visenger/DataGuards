5ffbcc6ab86aa9177ca513dec17b4d8061ac3547
visenger <visenger@gmail.com>
2014-09-05 12:49:09 +0200

    adding noise to data;

--- src/main/scala/de/data/preparation/NoiseInjector.scala
+++ src/main/scala/de/data/preparation/NoiseInjector.scala
@@ -5,13 +5,30 @@ package de.data.preparation
  */
 class NoiseInjector() {
 
-  def instance(path: String): this.type = {
-    //
+  import Seed._
+  import DataSet._
+
+  var seedType = None: Option[Seed]
+  var dataset = None: Option[DataSet]
+  var dataPath: String = ""
+  var noiseP: Int = 2
+
+
+  def tpch(path: String): this.type = {
+    dataset = Some(TPCH)
+    dataPath = path
     this
   }
 
+  def hosp(path: String): this.type = {
+    dataset = Some(HOSP)
+    dataPath = path
+    this
+  }
+
+
   def noisePercentage(n: Int): this.type = {
-    //
+    noiseP = n
     this
   }
 
@@ -20,15 +37,18 @@ class NoiseInjector() {
     this
   }
 
-  def noiseSeed(seed: String): this.type = {
-    //
+  def noiseSeed(seed: Seed): this.type = {
+    seedType = Some(seed)
     this
   }
 
-  def writeTo(path: String)={
-    //todo: perform the noise injection according to the introduced parameters;
-    //todo: write to file
-    //todo: write logs about noise: for evaluation
+  def writeTo(path: String) = {
+
+    dataset match {
+      case Some(TPCH) => TpchNoiseInjector.definedFor
+      case Some(HOSP) => HospNoiseInjector.definedFor
+      case _ => ???
+    }
   }
 
 
@@ -36,4 +56,40 @@ class NoiseInjector() {
 
 object NoiseInjector {
   def definedFor = new NoiseInjector
+}
+
+
+object Seed extends Enumeration {
+  type Seed = Value
+  val NUM, TEXT = Value
+}
+
+object DataSet extends Enumeration {
+  type DataSet = Value
+  val HOSP, TPCH = Value
+}
+
+
+class HospNoiseInjector {
+
+  //todo: perform the noise injection according to the introduced parameters;
+  //todo: write to file
+  //todo: write logs about noise: for evaluation
+
+}
+
+object HospNoiseInjector {
+  def definedFor = new HospNoiseInjector
+}
+
+class TpchNoiseInjector {
+
+  //todo: perform the noise injection according to the introduced parameters;
+  //todo: write to file
+  //todo: write logs about noise: for evaluation
+
+}
+
+object TpchNoiseInjector {
+  def definedFor = new TpchNoiseInjector
 }
