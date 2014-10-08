d244d825502c53fc08d9799f03da506fffb3e836
visenger <visenger@gmail.com>
2014-10-08 18:02:37 +0200

    hosp eval as latex table;

--- src/main/scala/de/result/evaluation/EvalHOSP.scala
+++ src/main/scala/de/result/evaluation/EvalHOSP.scala
@@ -65,94 +65,144 @@ object HOSPEvaluator extends App {
   val dirName = config.getString("data.hosp.resultFolder")
   val logFileName = "log-noise-hosp-1"
 
-  val evaluator = new Evaluator(Some(DataSet.HOSP), dirName, logFileName)
-  evaluator.runEvaluator
+  //  val evaluator = new Evaluator(Some(DataSet.HOSP), dirName, logFileName)
+  //  evaluator.runEvaluator
 
-  //  runEvaluator
-  //
-  //  def runEvaluator = {
-  //    val config = ConfigFactory.load()
-  //    val dirName = config.getString("data.hosp.resultFolder")
-  //    val logFileName = "log-noise-hosp-1"
-  //
-  //    for (i <- 2 to 10 if i % 2 == 0) {
-  //
-  //      val logData: List[String] = Source.fromFile(s"$dirName/$i/$logFileName-$i.tsv").getLines().toList
-  //      val logDataTuples: List[(Long, List[Int])] = convertLogData(logData)
-  //
-  //      println(s"_______________ % noise = $i ")
-  //
-  //      for {
-  //        attrFileName <- new File(s"$dirName/$i").listFiles().toIterator
-  //        if attrFileName.isFile && attrFileName.getName.startsWith("eq")
-  //      } {
-  //        val attrName: String = attrFileName.getName.takeWhile(_ != '-')
-  //
-  //        val attrNum: Int = HospTuple.getIdxByAttrName(attrName)
-  //
-  //        val containsAttrNum: List[(Long, List[Int])] = logDataTuples.filter(_._2.contains(attrNum))
-  //        val noisyIdx: List[Long] = containsAttrNum.map(_._1)
-  //        //println("attrFileName = " + attrName + "; attr num= " + attrNum + "; noise elements inserted = " + noisyIdx.size)
-  //
-  //        val inferred: List[String] = Source.fromFile(attrFileName).getLines().toList
-  //
-  //        //everything we found of form eqCityH(2459, MELROSE PARK, 2472, MELROSE PARKtypo)
-  //        //converted into (2459, MELROSE PARK), (2472, MELROSE PARKtypo)
-  //        val atoms: List[(AttrAtom, AttrAtom)] = convertToAttrAtoms(inferred)
-  //
-  //        //grouping on idx Long -> List[(AttrAtom,AttrAtom)] indexes and corresponding inferred atoms.
-  //        val structuredAtoms: List[(Long, List[(AttrAtom, AttrAtom)])] = noisyIdx.map(i => {
-  //          val idx: String = i.toString
-  //          val inferredVals: List[(AttrAtom, AttrAtom)] = atoms.filter(a => {
-  //            a._1.id == idx || a._2.id == idx
-  //          })
-  //          i -> inferredVals
-  //        })
-  //
-  //        // which idx were not found
-  //        val withEmptyList: List[(Long, List[(AttrAtom, AttrAtom)])] = structuredAtoms.filter(_._2.isEmpty)
-  //
-  //        val tp: Int = structuredAtoms.size - withEmptyList.size
-  //        val correct: Int = noisyIdx.size
-  //        println("noisy elements inserted = " + correct + " | found elements " + tp)
-  //        val precision = tp.toDouble / tp.toDouble
-  //        val recall = tp.toDouble / correct.toDouble
-  //        val f_measure = 2 * precision * recall / (precision + recall)
-  //        println(s"Attribute: $attrName precision=${precision} recall= ${recall} F measure= ${f_measure} \n")
-  //
-  //
-  //      }
-  //      //todo 2: for each %noise create an entry
-  //    }
-  //  }
-  //
-  //  def convertLogData(logData: List[String]): List[(Long, List[Int])] = {
-  //    val logDataTuples: List[(Long, List[Int])] = logData.map(l => {
-  //      val splitted: List[String] = l.split("\\t").toList
-  //      val idx: Long = convertToLong(splitted.head)
-  //      val attrNums: List[Int] = convertToInt(splitted.tail)
-  //      (idx, attrNums)
-  //    })
-  //    logDataTuples
-  //  }
-  //
-  //  def convertToAttrAtoms(input: List[String]): List[(AttrAtom, AttrAtom)] = {
-  //    val mapi: List[(AttrAtom, AttrAtom)] = input.map(a => {
-  //      val innerPart: String = a.substring(a.indexWhere(_ == '(') + 1, a.indexWhere(_ == ')'))
-  //      val Array(id1, val1, id2, val2) = innerPart.split(',')
-  //      Tuple2(AttrAtom(id1.trim, val1.trim), AttrAtom(id2.trim, val2.trim))
-  //    })
-  //    mapi
-  //  }
-  //
-  //
-  //  def convertToLong(s: String): Long = {
-  //    s.trim.toLong
-  //  }
-  //
-  //  def convertToInt(l: List[String]): List[Int] = {
-  //    l.map(_.trim.toInt)
-  //  }
+  runEvaluator
+
+  def runEvaluator = {
+
+
+    import Util._
+    val config = ConfigFactory.load()
+    val dirName = config.getString("data.hosp.resultFolder")
+    val logFileName = "log-noise-hosp-1"
+
+    /*eqAddressH eqCityH eqConditionH eqCountryNameH eqHospitalNameH eqMeasureNameH eqPhoneNumberH eqStateH eqZipCodeH*/
+    println( s"""\\multicolumn{1}{|c|}{\\%noi} & ADDR & CITY & COND & COUNT & HOSPNAME & MEASURE & PHONE & STATE & ZIP & \\multicolumn{1}{c|}{STATE} & \\multicolumn{1}{c|}{STATE}  \\\\ \\hline""")
+
+    for (i <- 2 to 10 if i % 2 == 0) {
+
+      val logData: List[String] = Source.fromFile(s"$dirName/$i/$logFileName-$i.tsv").getLines().toList
+      val logDataTuples: List[(Long, List[Int])] = convertLogData(logData)
+
+
+      var eqAddressH: Double = 0.0
+      var eqCityH: Double = 0.0
+      var eqConditionH: Double = 0.0
+      var eqCountryNameH: Double = 0.0
+      var eqHospitalNameH: Double = 0.0
+      var eqMeasureNameH: Double = 0.0
+      var eqPhoneNumberH: Double = 0.0
+      var eqStateH: Double = 0.0
+      var eqZipCodeH: Double = 0.0
+
+      //      println(s"_______________ % noise = $i ")
+
+      for {
+        attrFileName <- new File(s"$dirName/$i").listFiles().toIterator
+        if attrFileName.isFile && attrFileName.getName.startsWith("eq")
+      } {
+        val attrName: String = attrFileName.getName.takeWhile(_ != '-')
+
+        val attrNum: Int = HospTuple.getIdxByAttrName(attrName)
+
+        val containsAttrNum: List[(Long, List[Int])] = logDataTuples.filter(_._2.contains(attrNum))
+        val noisyIdx: List[Long] = containsAttrNum.map(_._1)
+        //println("attrFileName = " + attrName + "; attr num= " + attrNum + "; noise elements inserted = " + noisyIdx.size)
+
+        val inferred: List[String] = Source.fromFile(attrFileName).getLines().toList
+
+        //everything we found of form eqCityH(2459, MELROSE PARK, 2472, MELROSE PARKtypo)
+        //converted into (2459, MELROSE PARK), (2472, MELROSE PARKtypo)
+        val atoms: List[(AttrAtom, AttrAtom)] = convertToAttrAtoms(inferred)
+
+        //grouping on idx Long -> List[(AttrAtom,AttrAtom)] indexes and corresponding inferred atoms.
+        val structuredAtoms: List[(Long, List[(AttrAtom, AttrAtom)])] = noisyIdx.map(i => {
+          val idx: String = i.toString
+          val inferredVals: List[(AttrAtom, AttrAtom)] = atoms.filter(a => {
+            a._1.id == idx || a._2.id == idx
+          })
+          i -> inferredVals
+        })
+
+        // which idx were not found
+        val withEmptyList: List[(Long, List[(AttrAtom, AttrAtom)])] = structuredAtoms.filter(_._2.isEmpty)
+
+        val tp: Int = structuredAtoms.size - withEmptyList.size
+        val correct: Int = noisyIdx.size
+        //        println("noisy elements inserted = " + correct + " | found elements " + tp)
+        val precision = tp.toDouble / tp.toDouble
+        val recall = tp.toDouble / correct.toDouble
+        val f_measure = round(2 * precision * recall / (precision + recall))(4)
+        //        println(s"Attribute: $attrName precision=${precision} recall= ${recall} F measure= ${f_measure} \n")
+
+        attrName match {
+          case "eqAddressH" => {
+            eqAddressH = f_measure
+          }
+          case "eqCityH" => {
+            eqCityH = f_measure
+          }
+          case "eqConditionH" => {
+            eqConditionH = f_measure
+          }
+          case "eqCountryNameH" => {
+            eqCountryNameH = f_measure
+          }
+          case "eqHospitalNameH" => {
+            eqHospitalNameH = f_measure
+          }
+          case "eqMeasureNameH" => {
+            eqMeasureNameH = f_measure
+          }
+          case "eqPhoneNumberH" => {
+            eqPhoneNumberH = f_measure
+          }
+          case "eqStateH" => {
+            eqStateH = f_measure
+          }
+          case "eqZipCodeH" => {
+            eqZipCodeH = f_measure
+          }
+        }
+
+
+      }
+      //todo 2: for each %noise create an entry
+      /*eqAddressH eqCityH eqConditionH eqCountryNameH eqHospitalNameH eqMeasureNameH eqPhoneNumberH eqStateH eqZipCodeH*/
+
+      println( s"""\\multicolumn{1}{|l|}{$i}     & $eqAddressH  & $eqCityH  & $eqConditionH  & $eqCountryNameH & $eqHospitalNameH & $eqMeasureNameH & $eqPhoneNumberH  & $eqStateH  & $eqZipCodeH   & 0.4996 & $eqStateH \\\\ \\hline""")
+    }
+  }
+
+  def convertLogData(logData: List[String]): List[(Long, List[Int])] = {
+    val logDataTuples: List[(Long, List[Int])] = logData.map(l => {
+      val splitted: List[String] = l.split("\\t").toList
+      val idx: Long = convertToLong(splitted.head)
+      val attrNums: List[Int] = convertToInt(splitted.tail)
+      (idx, attrNums)
+    })
+    logDataTuples
+  }
+
+  def convertToAttrAtoms(input: List[String]): List[(AttrAtom, AttrAtom)] = {
+    val mapi: List[(AttrAtom, AttrAtom)] = input.map(a => {
+      val innerPart: String = a.substring(a.indexWhere(_ == '(') + 1, a.indexWhere(_ == ')'))
+      val Array(id1, val1, id2, val2) = innerPart.split(',')
+      Tuple2(AttrAtom(id1.trim, val1.trim), AttrAtom(id2.trim, val2.trim))
+    })
+    mapi
+  }
+
+
+  def convertToLong(s: String): Long = {
+    s.trim.toLong
+  }
+
+  def convertToInt(l: List[String]): List[Int] = {
+    l.map(_.trim.toInt)
+  }
 
 }
 
